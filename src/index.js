8b6e3c06cb3c3467103384138e04d49f716a242e
Rob Butcher <robbutcher2001@gmail.com>
2023-01-11 14:07:56 +0000

    Placeholder site

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 function render() {
+  const h1 = document.createElement("h1");
+  const heading = document.createTextNode("..in dev..");
+  h1.appendChild(heading);
+  h1.style.fontSize = "10vw";
+  h1.style.color = "#fff";
+
   const root = document.getElementById("root");
-  root.innerHTML = "Hello world, how are you? Does the update work?";
+  root.appendChild(h1);
+
+  document.body.style.fontFamily =
+    "-apple-system, 'Helvetica Neue', sans-serif";
+  document.body.style.backgroundColor = "#138595";
+
+  //height
+  document.querySelector("html").style.height = "100%";
+  document.body.style.height = "100%";
+  document.body.style.margin = "0";
+  root.style.height = "100%";
+
+  //title
+  root.style.display = "flex";
+  root.style.flexDirection = "column";
+  root.style.alignItems = "center";
+  root.style.justifyContent = "center";
 }
 
-render();
+document.body.onload = render;
